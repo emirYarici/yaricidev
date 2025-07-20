62293ad33d506bf60a2833bb3468c804d9e30e46
Emir Yarıcı <emiryarici@Emirs-MacBook-Pro.local>
2025-07-20 22:31:57 +0300

    home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
     <main className="flex flex-col flex-1 gap-12">
       <div className="flex flex-col flex-1 gap-5">
         <Link
+          href={"/start-transition"}
+          className=" flex-col w-full items-start duration-700 p-1 hover:scale-105  hover:translate-x-3 transition-all cursor-pointer flex gap-2"
+        >
+          <span className="font-bold text-2xl text-primary  ">
+            Should We Use StartTransition Api?
+          </span>
+          <span className="italic text-sm text-gray-300 ">20 Jul 2025</span>
+          <span className=" text-gray-300 ">Concurrency is good!</span>
+        </Link>
+        <Link
           href={"/react-native-fast-refresh"}
           className=" flex-col w-full items-start duration-700 p-1 hover:scale-105  hover:translate-x-3 transition-all cursor-pointer flex gap-2"
         >
@@ -15,17 +25,6 @@ export default function Home() {
           <span className=" text-gray-300 ">
             Module System, Hot Module Reload and more!
           </span>
-        </Link>
-
-        <Link
-          href={"/start-transition"}
-          className=" flex-col w-full items-start duration-700 p-1 hover:scale-105  hover:translate-x-3 transition-all cursor-pointer flex gap-2"
-        >
-          <span className="font-bold text-2xl text-primary  ">
-            Should We Use StartTransition Api?
-          </span>
-          <span className="italic text-sm text-gray-300 ">20 Jul 2025</span>
-          <span className=" text-gray-300 ">Concurrency is good!</span>
         </Link>
       </div>
     </main>
